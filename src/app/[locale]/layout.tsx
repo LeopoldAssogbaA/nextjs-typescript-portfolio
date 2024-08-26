b2876265faf63a0c6321068505cca4e87d196b9c
Leopold-WC <leopold.assogba@webcastor.fr>
2024-08-26 18:23:28 +0200

    remove script

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,7 +3,6 @@ import "../../../public/static/css/globals.css";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { Providers } from "../../providers";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "NextJs Typescript Portfolio",
@@ -28,10 +27,6 @@ export default async function RootLayout({
             {children}
           </NextIntlClientProvider>
         </Providers>
-        <Script
-          src="https://connect.soundcloud.com/sdk/sdk-3.3.2.js"
-          strategy="beforeInteractive"
-        />
       </body>
     </html>
   );

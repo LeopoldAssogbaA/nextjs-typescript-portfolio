fc68a1c0432fb84c9ad431f24eca0cb2d8a179cc
Leopold-WC <leopold.assogba@webcastor.fr>
2024-08-31 14:19:27 +0200

    test fix

--- src/utils/hooks/useInitPage.tsx
+++ src/utils/hooks/useInitPage.tsx
@@ -13,101 +13,75 @@ const useInitPage = (documentLoaded: boolean, init: boolean, setInit: (init: boo
 
     const initPage = () => {
       console.log('Fonction initPage appelée');
-      const waveOne = document.querySelector('.-one');
-      const waveTwo = document.querySelector('.-two');
-      const pageTransitionContainer = document.querySelector('div.page-transition-container-enter');
-      const aboutPresentation = document.querySelector(".about-presentation");
-      const mouseWheel = document.querySelector(".mouse-wheel");
-      const schoolContainer = document.querySelector(".school-container");
-      const workContainer = document.querySelector(".work-container");
-      const contactContainer = document.querySelector(".contact-container");
-      const name = document.querySelector('.name');
+      gsap.fromTo('.name',
+        { opacity: 0, y: -55 },
+        { 
+          y: 0, 
+          opacity: 1, 
+          duration: 1, 
+          delay: 1, 
+          ease: "back.inOut(2)",
+          onComplete: () => console.log("Animation 'name' terminée")
+        }
+      );
 
-      if (name) {
-        gsap.fromTo(name,
-          { opacity: 0, y: -55 },
-          { 
-            y: 0, 
-            opacity: 1, 
-            duration: 1, 
-            delay: 1, 
-            ease: "back.inOut(2)",
-            onComplete: () => console.log("Animation 'name' terminée")
-          }
-        );
-      }
+      gsap.fromTo('div.page-transition-container-enter',
+        { zIndex: -1 },
+        { 
+          zIndex: 2,
+          onComplete: () => console.log("Animation 'pageTransitionContainer' terminée")
+        }
+      );
 
-      if (pageTransitionContainer) {
-        gsap.fromTo(pageTransitionContainer,
-          { zIndex: -1 },
-          { 
-            zIndex: 2,
-            onComplete: () => console.log("Animation 'pageTransitionContainer' terminée")
-          }
-        );
-      }
+      gsap.fromTo(".about-presentation",
+        { display: "none" },
+        { display: "block" }
+      );
 
-      if (aboutPresentation) {
-        gsap.fromTo(aboutPresentation,
-          { display: "none" },
-          { display: "block" }
-        );
-      }
+      gsap.fromTo(
+        ".mouse-wheel",
+        { display: "none", opacity: 0, zIndex: -2, top: "40%" },
+        { zIndex: 10, top: "46%", display: "block", opacity: 1, duration: .8, ease: "back.inOut(1)", delay: 1.2 }
+      );
 
-      if (mouseWheel) {
-        gsap.fromTo(
-          mouseWheel,
-          { display: "none", opacity: 0, zIndex: -2, top: "40%" },
-          { zIndex: 10, top: "46%", display: "block", opacity: 1, duration: .8, ease: "back.inOut(1)", delay: 1.2 }
-        );
-      }
+      gsap.fromTo(".school-container",
+        { display: "none" },
+        { display: "initial" }
+      );
 
-      if (schoolContainer) {
-        gsap.fromTo(schoolContainer,
-          { display: "none" },
-          { display: "initial" }
-        );
-      }
+      gsap.fromTo(".work-container",
+        { display: "none" },
+        { display: "initial" }
+      );
 
-      if (workContainer) {
-        gsap.fromTo(workContainer,
-          { display: "none" },
-          { display: "initial" }
-        );
-      }
+      gsap.fromTo(".contact-container",
+        { display: "none" },
+        { display: "flex" }
+      );
 
-      if (contactContainer) {
-        gsap.fromTo(contactContainer,
-          { display: "none" },
-          { display: "flex" }
-        );
-      }
+      gsap.fromTo('.-one',
+        { opacity: 0, left: '-110%', top: '-120%' },
+        { 
+          opacity: 0.4, 
+          left: '-50%', 
+          top: '-60%', 
+          duration: 2.5, 
+          ease: "back.inOut",
+          onComplete: () => console.log("Animation 'waveOne' terminée")
+        }
+      );
 
-      if (waveOne && waveTwo) {
-        gsap.fromTo(waveOne,
-          { opacity: 0, left: '-110%', top: '-120%' },
-          { 
-            opacity: 0.4, 
-            left: '-50%', 
-            top: '-60%', 
-            duration: 2.5, 
-            ease: "back.inOut",
-            onComplete: () => console.log("Animation 'waveOne' terminée")
-          }
-        );
-
-        gsap.fromTo(waveTwo,
-          { opacity: 0, left: '-110%', top: '-120%' },
-          { 
-            opacity: 0.5, 
-            left: '-50%', 
-            top: '-60%', 
-            duration: 2.6, 
-            ease: "back.inOut",
-            onComplete: () => console.log("Animation 'waveTwo' terminée")
-          }
-        );
-      }
+      gsap.fromTo('.-two',
+        { opacity: 0, left: '-110%', top: '-120%' },
+        { 
+          opacity: 0.5, 
+          left: '-50%', 
+          top: '-60%', 
+          duration: 2.6, 
+          ease: "back.inOut",
+          onComplete: () => console.log("Animation 'waveTwo' terminée")
+        }
+      );
       setInit(true);
     };
 

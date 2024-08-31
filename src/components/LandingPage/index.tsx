bf3d3b22fe941c785714f48439152d5740e1e346
Leopold-WC <leopold.assogba@webcastor.fr>
2024-08-31 14:35:38 +0200

    revert useInitPage

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import {
   Name,
   NameContainer,
@@ -11,6 +11,7 @@ import { useTranslations } from 'next-intl';
 import useLandingScrollAnimations from '../../utils/hooks/useLandingScrollAnimations';
 import useInitPage from '../../utils/hooks/useInitPage';
 import useWaveScroll from '../../utils/hooks/useWaveScroll';
+import gsap from 'gsap';
 
 const LandingPage: React.FC<{
   setInit: (init: boolean) => void,
@@ -23,8 +24,160 @@ const LandingPage: React.FC<{
 }) => {
   const t = useTranslations('Home');
   useLandingScrollAnimations();
-  useInitPage(documentLoaded, init, setInit);
+  // useInitPage(documentLoaded, init, setInit);
   useWaveScroll();
+
+  useLayoutEffect(() => {
+    if (typeof window === 'undefined' || !documentLoaded || init) {
+      return;
+    }
+
+    const initPage = () => {
+      const waveOne = document.querySelector('.-one');
+      const waveTwo = document.querySelector('.-two');
+      const pageTransitionContainer = document.querySelector('div.page-transition-container-enter');
+      const aboutPresentation = document.querySelector(".about-presentation");
+      const mouseWheel = document.querySelector(".mouse-wheel");
+      const schoolContainer = document.querySelector(".school-container");
+      const workContainer = document.querySelector(".work-container");
+      const contactContainer = document.querySelector(".contact-container");
+      const name = document.querySelector('.name');
+
+
+      if (name) {
+        gsap.fromTo(name, 
+          {
+            opacity: 0,
+            y: -55,
+          },
+          {
+            y: 0,
+            opacity: 1,
+            duration: 1,
+            delay: 1,
+            ease: "back.inOut(2)",
+            onComplete: () => {
+              console.log("Animation du nom terminée");
+            }
+          });
+      }
+
+      if (pageTransitionContainer) {
+        gsap.fromTo(pageTransitionContainer, 
+          {
+            zIndex: -1,
+          },
+          {
+            zIndex: 2,
+            onComplete: () => {
+            console.log("Animation du conteneur de transition de page terminée");
+          }
+        });
+      }
+
+      if (aboutPresentation) {
+        gsap.fromTo(aboutPresentation, 
+          {
+            display: "none",
+          },
+          {
+            display: "block",
+            onComplete: () => {
+            console.log("Animation de la présentation 'about' terminée");
+          }
+        });
+      }
+
+      if (mouseWheel) {
+
+        gsap.fromTo(
+          mouseWheel,
+          { display: "none", opacity: 0, zIndex: -2, top: "40%", },
+          {
+            zIndex: 10, top: "46%", display: "block", opacity: 1, duration: .8, ease: "back.inOut(1)", delay: 1.2,
+            onComplete: () => {
+              console.log("Animation de fondu de la roue de souris terminée");
+            }
+          }
+        );
+      }
+
+      if (schoolContainer) {
+        gsap.fromTo(schoolContainer, 
+          {
+            display: "none",
+          },
+          {
+            display: "initial",
+            onComplete: () => {
+            console.log("Animation du conteneur d'école terminée");
+          }
+        });
+      }
+
+      if (workContainer) {
+        gsap.fromTo(workContainer, 
+          {
+            display: "none",
+          },
+          {
+            display: "initial",
+            onComplete: () => {
+            console.log("Animation du conteneur de travail terminée");
+          }
+        });
+      }
+
+      if (contactContainer) {
+        gsap.fromTo(contactContainer, 
+          {
+            display: "none",
+          },
+          {
+            display: "flex",
+            onComplete: () => {
+            console.log("Animation du conteneur de contact terminée");
+          }
+        });
+      }
+
+      if (waveOne && waveTwo) {
+        gsap.fromTo(waveOne, {
+          opacity: 0,
+          left: '-110%',
+          top: '-120%',
+        }, {
+          opacity: 0.4,
+          left: '-50%',
+          top: '-60%',
+          duration: 2.5,
+          ease: "back.inOut",
+          onComplete: () => {
+            console.log("Animation de la vague terminée");
+          }
+        });
+      
+        gsap.fromTo(waveTwo, {
+          opacity: 0,
+          left: '-110%',
+          top: '-120%',
+        }, {
+          opacity: 0.5,
+          left: '-50%',
+          top: '-60%',
+          duration: 2.6,
+          ease: "back.inOut",
+          onComplete: () => {
+            console.log("Animation de la vague terminée");
+          }
+        });
+      }
+
+      setInit(true);
+    }
+
+    initPage();
+  }, [init, documentLoaded, setInit]);
   
   return (
     <LandingPageContainer className="landing-page">

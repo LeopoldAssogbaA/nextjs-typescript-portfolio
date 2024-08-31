774289f4c36b2b6843176186679751712179ca00
Leopold-WC <leopold.assogba@webcastor.fr>
2024-08-31 14:30:53 +0200

    test fix

--- src/utils/hooks/useRevealTransitionAnimations.tsx
+++ src/utils/hooks/useRevealTransitionAnimations.tsx
@@ -4,26 +4,26 @@ import { useEffect } from 'react';
 
 const useRevealTransitionAnimations = () => {
   const { contextSafe } = useGSAP();
-  
-  useEffect(() => {
-    const animateReveal = contextSafe(() => {
-      const tl = gsap.timeline();
-      tl.to('.reveal-transition', {
-        duration: 1,
-        clipPath: 'inset(100% 0% 0% 0%)',
-        stagger: 0.1,
-        ease: 'back.inOut(2)',
-        onComplete: () => {
-          gsap.to('.reveal-transition-container', {
-            duration: 0,
-            visibility: "hidden",
-          });
-        }
-      });
+
+  const animateReveal = contextSafe(() => {
+    const tl = gsap.timeline();
+    tl.to('.reveal-transition', {
+      duration: 1,
+      clipPath: 'inset(100% 0% 0% 0%)',
+      stagger: 0.1,
+      ease: 'back.inOut(2)',
+      onComplete: () => {
+        gsap.to('.reveal-transition-container', {
+          duration: 0,
+          visibility: "hidden",
+        });
+      }
     });
-  
-    animateReveal();
   });
+
+  useEffect(() => {
+    animateReveal();
+  }, [animateReveal]);
 };
 
 export default useRevealTransitionAnimations;
